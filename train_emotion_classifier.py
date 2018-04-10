9c1f042337a671dd1116c10ef63a2f8617efaf7a
Rajiv Vijayakumar <rvijayak@qti.qualcomm.com>
2018-04-10 15:59:00 -0700

    More layers

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -84,9 +84,15 @@ model.add(
     Conv2D(64, (3, 3), padding="same", activation="relu")
 )
 model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(
+    Conv2D(128, (3, 3), padding="same", activation="relu")
+)
+model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(100, activation="relu"))
-model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation="softmax"))
 
 
