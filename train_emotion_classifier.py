e2ed6f4a2c7b474effd35987d6ffdd53c49e2481
Rajiv Vijayakumar <rvijayak@qti.qualcomm.com>
2018-04-10 16:53:11 -0700

    add dropout only in the middle

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -79,21 +79,21 @@ model.add(
     Conv2D(64, (3, 3), padding="same", input_shape=input_shape, activation="relu")
 )
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+# model.add(Dropout(0.25))
 model.add(
     Conv2D(128, (3, 3), padding="same", activation="relu")
 )
 model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.25))
+model.add(Dropout(0.25))
 model.add(
     Conv2D(256, (3, 3), padding="same", activation="relu")
 )
-# model.add(Dropout(0.25))
+model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(256, activation="relu"))
-# model.add(Dropout(0.3))
-model.add(Dense(256, activation="relu"))
 model.add(Dropout(0.3))
+model.add(Dense(256, activation="relu"))
+# model.add(Dropout(0.3))
 model.add(Dense(num_classes, activation="softmax"))
 
 
